2bac037209a01c8fc9b58c2bbd99fc8611e6b0f8
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2011-04-17 18:22:09 +0300

    Check if redirection is needed.

--- exec.c
+++ exec.c
@@ -16,10 +16,14 @@ void issuecmd(char *name, char **argv, char **envp)
 	assert(name);
 	argv[0] = name;
 
-	dup2(fdin, 0);
-	close(fdin);
-	dup2(fdout, 1);
-	close(fdout);
+	if (-1 != fdin) {
+		dup2(fdin, 0);
+		close(fdin);
+	}
+	if (-1 != fdout) {
+		dup2(fdout, 1);
+		close(fdout);
+	}
 
 	execve(name, argv, envp);
 	perror("execve");
